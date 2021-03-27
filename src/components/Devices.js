48b8db672c232a7daa78bb6391249b513236e099
junaama <0naama0@gmail.com>
2021-03-27 17:40:11 -0400

    (feat): add basic styling to pages

--- src/components/Devices.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Door from "./Door/Door";
-import Camera from "./Camera/Camera";
-//**Render Door or Camera route */
-const Devices = () => {
-  return <></>;
-};
-
-export default Devices;
